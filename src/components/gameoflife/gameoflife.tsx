cf214c3be8fbe2a914e105aa6800116162e9efaf
Ethan Lee <ethantlee21@gmail.com>
2025-09-19 15:37:26 -0400

    enhancement: improve canvas rendering for high dpi screens
    - Updated canvas rendering to account for device pixel ratio for sharper visuals on high dpi screens.
    - Adjusted canvas width and height to match the device pixel ratio.

--- src/components/gameoflife/gameoflife.tsx
+++ src/components/gameoflife/gameoflife.tsx
@@ -84,11 +84,17 @@ const GameOfLife = memo(() => {
     if (!canvas || !container || matrix.length === 0) return;
 
     const { width, height } = container.getBoundingClientRect();
-    canvas.width = width;
-    canvas.height = height;
+    const dpr = window.devicePixelRatio || 1;
+
+    canvas.width = width * dpr;
+    canvas.height = height * dpr;
+    canvas.style.width = `${width}px`;
+    canvas.style.height = `${height}px`;
 
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
+
+    ctx.scale(dpr, dpr);
 
     const style = getComputedStyle(container);
     const textColor = style.getPropertyValue("--secondary");
